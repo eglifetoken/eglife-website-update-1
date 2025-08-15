5c9b643581504334ccca778ccb69f663058599bf
bhagwan kumar <bk15011987@gmail.com>
2025-08-15 09:12:31 +0000

    remove whit patti from header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -62,7 +62,7 @@ export default function Header() {
   );
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b">
       <div className="container mx-auto flex h-20 items-center justify-between px-4 md:px-6">
         <Link href="/" className="flex items-center gap-3">
           <Logo />
