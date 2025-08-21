689ebe3752849c7bb252c3905cfc57033bd6852b
bhagwan kumar <bk15011987@gmail.com>
2025-08-21 03:08:10 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAQRIpGu8u0gym566nK4Xfu2LcxSih9XDU",
   authDomain: "egli-hub.firebaseapp.com",
   projectId: "egli-hub",
-  storageBucket: "egli-hub.appspot.com",
+  storageBucket: "egli-hub.firebasestorage.app",
   messagingSenderId: "669798404623",
   appId: "1:669798404623:web:a92835d0a1a36ea6e6897a"
 };
