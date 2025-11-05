945338ddb4b858e4ccda87d709e28e4926bf95ac
bhagwan kumar <bk15011987@gmail.com>
2025-11-05 12:16:38 +0000

    5:42:37 PM: Netlify Build

--- next.config.ts
+++ next.config.ts
@@ -31,8 +31,17 @@ const nextConfig: NextConfig = {
       }
     ],
   },
-   webpack: (config) => {
+   webpack: (config, { isServer }) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
+    if (!isServer) {
+        config.resolve.fallback = {
+            ...config.resolve.fallback,
+            fs: false,
+            net: false,
+            tls: false,
+            http2: false,
+        };
+    }
     return config;
   },
 };
