0c191e9a8392c406632f591076c0367b69a6d620
bhagwan kumar <bk15011987@gmail.com>
2025-11-07 10:54:19 +0000

    sirf metamask connect ho raha hai jab ki koi sa bhi wallet connect hona

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,12 +9,12 @@ import Footer from '@/components/layout/footer';
 import { WagmiProvider, createConfig, http } from 'wagmi';
 import { bsc } from 'wagmi/chains';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { metaMask } from 'wagmi/connectors';
+import { injected } from 'wagmi/connectors';
 
 
 const config = createConfig({
   chains: [bsc],
-  connectors: [metaMask()],
+  connectors: [injected()],
   transports: {
     [bsc.id]: http(),
   },
