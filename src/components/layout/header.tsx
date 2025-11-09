87c671f2f14c39da62b1dbf074746a6fd48f6c49
bhagwan kumar <bk15011987@gmail.com>
2025-11-09 04:41:13 +0000

    upper eglife token likha hai sayad logo hai use hatao

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -34,9 +34,7 @@ export default function Header() {
     <header className="absolute top-0 z-50 w-full bg-transparent">
       <div className="container mx-auto flex h-20 items-center justify-between px-4 md:px-6">
         
-        <Link href="/" className="font-headline text-xl font-bold text-white">
-            EGLIFE TOKEN
-        </Link>
+        <div></div>
         
         <div className="flex items-center lg:hidden">
            <Sheet open={isMobileMenuOpen} onOpenChange={setIsMobileMenuOpen}>
@@ -76,5 +74,3 @@ export default function Header() {
     </header>
   );
 }
-
-    
