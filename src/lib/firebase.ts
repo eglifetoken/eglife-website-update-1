064d526eb0d3fa9b841d76f770dfb7e2deb04481
bhagwan kumar <bk15011987@gmail.com>
2025-11-14 01:40:41 +0000

    transaction nahi ho rha hai submitting hi ho raha hai bahut der se

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,12 +7,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAQRIpGu8u0gym566nK4Xfu2LcxSih9XDU",
-  authDomain: "egli-hub.firebaseapp.com",
-  projectId: "egli-hub",
-  storageBucket: "egli-hub.firebasestorage.app",
-  messagingSenderId: "669798404623",
-  appId: "1:669798404623:web:a92835d0a1a36ea6e6897a"
+  apiKey: "AIzaSyD7siVG6oM8wOTGL0ZOlzqJsNsFDUF7sl0",
+  authDomain: "eglife-token-1e4c5.firebaseapp.com",
+  projectId: "eglife-token-1e4c5",
+  storageBucket: "eglife-token-1e4c5.appspot.com",
+  messagingSenderId: "321177983654",
+  appId: "1:321177983654:web:fcb2a47126845d22117267",
+  measurementId: "G-Y681N12MWG"
 };
 
 // Initialize Firebase
